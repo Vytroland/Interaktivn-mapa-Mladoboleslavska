5a0f5102781b734dee8287cfd4ed546ae0690138
Vytroland <149309667+Vytroland@users.noreply.github.com>
2026-03-29 23:26:58 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const locations = {
-    "mnichovo": { title: "Mnichovo Hradiště", desc: "Barokní sídlo Valdštejnů s rozsáhlou knihovnou.", img: "Minchovo.jpg" },
-    "bela": { title: "Bělá pod Bezdězem", desc: "Historický zámek s expozicí o historii regionu a papírenství.", img: "bela.jpg" },
+    "mnichovo": { title: "Mnichovo Hradiště", desc: "Barokní perla Valdštejnů Velkolepá rezidence, která vás přenese do éry Albrechta z Valdštejna. Mezi největší lákadla patří unikátní zámecké divadlo s původní mašinerií a rozsáhlá knihovna, o kterou se kdysi staral slavný Giacomo Casanova.", img: "Minchovo.jpg" },
+    "bela": { title: "Bělá pod Bezdězem", desc: "Brána do kraje hradů Původně tvrz přestavěná na renesanční a později barokní zámek. Je úzce spjat s rodem Valdštejnů a nabízí expozice věnované panství, lesnictví i papírenství. Zámek tvoří výraznou dominantu náměstí a je ideálním startovním bodem pro výlety směrem k hradu Bezděz.", img: "bela.jpg" },
     "michalovice": { title: "Michalovická Putna", desc: "Zřícenina hradu proslulá svou šikmou věží zvanou Putna.", img: "Putna.jpg" },
-    "loucen": { title: "Zámek Loučen", desc: "Unikátní zámecký areál s mnoha labyrinty a bludišti.", img: "Loucen.jpg" }
+    "loucen": { title: "Zámek Loučen", desc: "Labyrinty a bludiště Barokní zámek obklopený rozsáhlým parkem, který ukrývá evropský unikát – 12 různých labyrintů a bludišť. Kromě zábavy v zahradě nabízí zámek i pohled do života šlechtického rodu Thurn-Taxisů.", img: "Loucen.jpg" }
 };
 
 function hidePanels() {
